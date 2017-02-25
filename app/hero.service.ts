024cab3278bfeba4c510eb0157c20d489a5539c7
ekansh <mail.ekansh@gmail.com>
2017-02-25 00:11:03 -0500

    Made modification in the code structure.

--- app/hero.service.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable } from '@angular/core';
-import { HEROES } from './mock-heros';
-import { Hero } from './hero';
-@Injectable()
-export class HeroService{
-	getHeros(): Hero[]{
-		return HEROES;
-	}
-}
---
+++ src/app/app.component.ts
@@ -0,0 +1,105 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Hero } from './hero';
+import { HeroService } from './hero.service';
+import { OnInit } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  template: `
+   
+  <my-hero-detail [hero]="selectedHero"></my-hero-detail>
+    
+    <div>
+      
+      <ul class="heroes">
+        <li *ngFor="let hero of heroes" [class.selected]="hero === selectedHero" (click)="onSelect(hero)" >
+         <span class="badge" >{{hero.id}}</span> 
+            {{hero.name}}
+        </li>
+      </ul>
+    </div>
+  `,
+  providers: [HeroService],
+  styles: [`
+  .selected {
+    background-color: #CFD8DC !important;
+    color: white;
+  }
+  .heroes {
+    margin: 0 0 2em 0;
+    list-style-type: none;
+    padding: 0;
+    width: 15em;
+  }
+  .heroes li {
+    cursor: pointer;
+    position: relative;
+    left: 0;
+    background-color: #EEE;
+    margin: .5em;
+    padding: .3em 0;
+    height: 1.6em;
+    border-radius: 4px;
+  }
+  .heroes li.selected:hover {
+    background-color: #BBD8DC !important;
+    color: white;
+  }
+  .heroes li:hover {
+    color: #607D8B;
+    background-color: #DDD;
+    left: .1em;
+  }
+  .heroes .text {
+    position: relative;
+    top: -3px;
+  }
+  .heroes .badge {
+    display: inline-block;
+    font-size: small;
+    color: white;
+    padding: 0.8em 0.7em 0 0.7em;
+    background-color: #607D8B;
+    line-height: 1em;
+    position: relative;
+    left: -1px;
+    top: -4px;
+    height: 1.8em;
+    margin-right: .8em;
+    border-radius: 4px 0 0 4px;
+  }
+`]
+
+})
+export class AppComponent implements OnInit {
+
+  title = 'Tour of Heroes';
+  heroes: Hero[];
+  selectedHero: Hero;
+  
+  constructor(private heroService: HeroService) {
+    console.log("calling AppComponent constructor");
+    //this.heroes = this.heroService.getHeros();
+  }
+
+  ngOnInit(): void{
+    console.log("calling ngOnInit ")
+    this.getHeroes();
+  }
+  getHeroes(): void{
+    this.heroService.getHeros().then(
+     heroes=>this.heroes=heroes
+      //this.aFUnction
+    );
+    
+  }
+  aFUnction(her: Hero[]): void {
+    console.log("aFUnction");
+    this.heroes= her;
+
+  }
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+}
